c27782d100c3f11fc3eb49eebd3938093cd64830
Test <test@Tests-MacBook-Pro.local>
2014-11-24 15:42:56 +0530

    Initial Commit

---
+++ Shadow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shadow
+//
+//  Created by Test on 24/11/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shadow/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Shadow
+//
+//  Created by Test on 24/11/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
